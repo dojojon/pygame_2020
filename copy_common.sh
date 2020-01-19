eaaae9df62057f5afa199adb4195ea8d45f1c718
Dojo Jon <dojo@outlook.ie>
2020-01-19 08:22:07 +0000

    Add copy commotn scrip

---
+++ copy_common.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 'Copy common files to game dirs'
+
+function copy_common(){
+    echo 'Creating Common '$1
+    mkdir -p ./$1/common/
+    cp -v ./particles/particle_engine.py ./$1/common/.
+}
+
+echo 'Lemon Leak'
+
+copy_common lemon_leak
