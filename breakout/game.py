e993ac7b70bae2eec540765771992b9f6d42dd03
Dojo Jon <dojo@outlook.ie>
2020-06-21 10:53:41 +0100

    breakout - bat add

--- breakout/game.py
+++ breakout/game.py
@@ -14,6 +14,11 @@ ball.y = 400
 ball.vx = randrange(-20, 20)
 ball.vy = randrange(-20, -5)
 
+bat = Actor("breakout_bat")
+bat.top =HEIGHT - 32
+bat.left =(WIDTH - bat.width)/2
+bat.speed = 200
+
 def update_ball(dt):
     global ball
 
@@ -35,14 +40,27 @@ def update_ball(dt):
     if ball.bottom > HEIGHT:
         ball.vy *= -1
 
+def update_bat(dt):
+
+    if keyboard.left:
+        bat.x -= dt * bat.speed
+    if keyboard.right:
+        bat.x += dt * bat.speed
+
+    if bat.left < 0:
+        bat.left = 0
+    if bat.right > WIDTH:
+        bat.right = WIDTH
 
 def update(dt):
     update_ball(dt)
+    update_bat(dt)
     pass
 
 def draw():
     screen.fill((0, 10, 30))
     screen.draw.text("Lives: {0}".format(lives), (20, 20))
+    bat.draw()
     ball.draw()
     pass
 
